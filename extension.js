c5bedaf09ee4eadc2c10af3cb3bb33a7ca3e7165
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2025-07-02 22:12:35 -0400

    Remove tab characters

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require('vscode')
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,27 +10,27 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-auto-bullets" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "markdown-auto-bullets" is now active!')
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('markdown-auto-bullets.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand('markdown-auto-bullets.helloWorld', function () {
+    // The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Markdown Automatic Bullet Points!');
-	});
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from Markdown Automatic Bullet Points!')
+  })
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
-	activate,
-	deactivate
+  activate,
+  deactivate
 }
