8d1d4cc54ddbbf4b72d88bf35d16a452f4e39eda
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2025-07-06 22:22:15 -0400

    Update configs and readme; add ignores to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,16 @@ import globals from "globals"
 import js from "@eslint/js"
 
 export default [
-  js.configs.recommended,
+  js.configs.all,
   {
-    files: ["**/*.js"],
+    ignores: [
+      ".vscode-test/**",
+      ".vscode/**",
+      "node_modules/**"
+    ]
+  },
+  {
+    files: ["**/*.js", "!**/*.test.js"],
     languageOptions: {
       ecmaVersion: 2022,
       globals: {
@@ -13,5 +20,33 @@ export default [
         ...globals.mocha,
       },
       sourceType: "module",
+    },
+    rules: {
+      "curly": ["off"],
+      "eqeqeq": ["off"],
+      "func-style": ["off"],
+      "max-params": ["warn"],
+      "max-statements": ["warn"],
+      "no-console": ["off"],
+      "no-implicit-coercion": ["warn"],
+      "no-inline-comments": ["off"],
+      "no-magic-numbers": ["error", { "ignore": [0, 1] }],
+      "no-negated-condition": ["warn"],
+      "no-promise-executor-return": ["warn"],
+      "no-ternary": ["off"],
+      "no-use-before-define": ["off"],
+      "no-warning-comments": ["warn"],
+      "one-var": ["off"],
+      "prefer-named-capture-group": ["off"],
+    },
+  },
+  {
+    files: ["**/*.test.js"],
+    rules: {
+      "max-lines-per-function": ["off"],
+      "max-params": ["off"],
+      "max-statements": ["off"],
+      "no-magic-numbers": ["off"],
     }
-  }]
+  }
+]
