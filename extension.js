5c42c62c13ac0d27048fcfd746fefd9823103539
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2025-07-06 23:32:33 -0400

    Add main method to extension and related tests; set eslint to recommended rules

--- extension.js
+++ extension.js
@@ -7,12 +7,40 @@ const RETURN = '\n'
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand('type', async (args) => {
-      console.log('Markdown Auto Bullets: type command triggered', args)
-      await vscode.commands.executeCommand('default:type', args)
-    })
-  )
+  const eventRegistration = vscode.commands.registerCommand('type', async (args) => {
+    addBullet(args)
+    await vscode.commands.executeCommand('default:type', args)
+  })
+  context.subscriptions.push(eventRegistration)
+}
+
+function addBullet(args) {
+  try {
+    console.log(`args: ${JSON.stringify(args)}`)
+
+    const editor = vscode.window.activeTextEditor
+    if (!isMarkdown(editor)) {
+      return
+    }
+    console.log('>> Passed isMarkdown check')
+
+    if (!isEnterKey(args)) return
+    console.log('>> Passed isEnterKey check')
+
+    const cursor = getCursor(editor)
+    const lineText = getText(editor, cursor.line)
+    if (!lineText) return
+    console.log('>> Passed getText check')
+
+    const bullet = getBullet(lineText)
+    if (!bullet) return
+    console.log('>> Passed getBullet check')
+
+    args.text += bullet
+    console.log('>> Added bullet to args.text:', args.text)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function deactivate() { }
@@ -20,6 +48,22 @@ function deactivate() { }
 function getBullet(text) {
   const match = text.match(BULLET_REGEX)
   return match && (match.length > 1) ? match[1] : null
+}
+
+function getCursor(editor) {
+  return editor.selection.active
+}
+
+function getText(editor, line) {
+  return editor.document.lineAt(line).text
+}
+
+function isEnterKey(args) {
+  return args.text == RETURN
+}
+
+function isMarkdown(editor) {
+  return editor.document.languageId == 'markdown'
 }
 
 module.exports = {
