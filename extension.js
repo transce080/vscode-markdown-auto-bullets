5b5096ab0ad312300c6b09f6cba6cabe2af840c5
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2025-07-06 21:24:21 -0400

    Add type event

--- extension.js
+++ extension.js
@@ -1,33 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "markdown-auto-bullets" is now active!')
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('markdown-auto-bullets.helloWorld', function () {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from Markdown Automatic Bullet Points!')
-  })
-
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(
+    vscode.commands.registerCommand('type', async (args) => {
+      console.log('Markdown Auto Bullets: type command triggered', args)
+      await vscode.commands.executeCommand('default:type', args)
+    })
+  )
 }
 
-// This method is called when your extension is deactivated
 function deactivate() { }
 
 module.exports = {
