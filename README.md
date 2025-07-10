89e2abe1dc7eb8b6993ca1584bcbee73cfe64159
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2025-07-09 23:42:26 -0400

    Update configs and readme

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # VSCode Markdown Automatic Bullet Points
 
-An open-source "micro-extension" which automatically adds bullet points when editing in Markdown.
+An open-source "mini-extension" which automatically adds bullet points when editing in Markdown.
 
 ## Features
 
-- Detects the type of bullet point and spacing
 - Automatically inserts bullet point when starting new line
-
-## Requirements
-
-- None
+- Detects the type of bullet point and spacing
+- Turns off the type event when not in markdown to reduce resource use
 
 ## Release Notes
 
@@ -17,6 +14,14 @@ An open-source "micro-extension" which automatically adds bullet points when edi
 
 Initial release
 
+## Known Issues
+
+- After changing the document language to markdown, the user must type at least one character before the event rewires itself. If the first key pressed is Enter, the extension will not add a bullet point. This should not be an issue under normal circumstances.
+
 ## Author
 
 - Jason Lonsberry (transce080)
+
+## Repository
+
+- [https://github.com/transce080/vscode-markdown-auto-bullets](https://github.com/transce080/vscode-markdown-auto-bullets.git/)
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,9 @@ import globals from "globals"
 import js from "@eslint/js"
 
 export default [
-  js.configs.recommended,
+  js.configs.all,
   {
-    files: ["**/*.js"],
+    files: ["**/*.js", "!**/*.test.js"],
     languageOptions: {
       ecmaVersion: 2022,
       globals: {
@@ -13,5 +13,32 @@ export default [
         ...globals.mocha,
       },
       sourceType: "module",
+    },
+    rules: {
+      "curly": ["off"],
+      "eqeqeq": ["off"],
+      "func-style": ["off"],
+      "max-params": ["warn"],
+      "max-statements": ["warn"],
+      "no-console": ["off"],
+      "no-implicit-coercion": ["warn"],
+      "no-inline-comments": ["off"],
+      "no-magic-numbers": ["error", { "ignore": [0, 1] }],
+      "no-promise-executor-return": ["warn"],
+      "no-ternary": ["off"],
+      "no-use-before-define": ["off"],
+      "no-warning-comments": ["warn"],
+      "one-var": ["off"],
+      "prefer-named-capture-group": ["off"],
+    },
+  },
+  {
+    files: ["**/*.test.js"],
+    rules: {
+      "max-lines-per-function": ["off"],
+      "max-params": ["off"],
+      "max-statements": ["off"],
+      "no-magic-numbers": ["off"],
     }
-  }]
+  }
+]
