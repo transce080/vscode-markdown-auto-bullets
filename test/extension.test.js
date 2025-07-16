280dd7b52bddd5f9526ffa3062ccb8f861b746bc
Jason Lonsberry <33551248+transce080@users.noreply.github.com>
2025-07-15 23:43:06 -0400

    Change onDidChangeTextDocument event to onDidOpenTextDocument; added properties to package.json

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,10 +1,9 @@
 const { commands, languages, window, workspace } = require('vscode')
-const { EndOfLine, Selection } = require('vscode')
-const { getBullet, RETURN } = require('../src/extension')
+const { CRLF, LF, Selection } = require('vscode')
+const { testExports } = require('../src/extension')
 const assert = require('assert')
 
-const { LF } = EndOfLine
-const { CRLF } = EndOfLine
+const { getBullet, RETURN } = testExports
 const WAIT_TIME = 150 // Increase wait time if you experience intermittent failures or for slower machines
 
 const testDepthOne = '  * Lorem Ipsum'
@@ -17,7 +16,7 @@ const testNoBulletsWithPlus = 'Lorem + Ipsum Dolor Sit Amet'
 const testNoBulletsWithStar = 'Lorem Ipsum Dolor * Sit Amet'
 
 async function runTest(content, language = 'markdown', lineEnding = LF, secondLanguage = null) {
-  let document = await workspace.openTextDocument({ content, language })
+  const document = await workspace.openTextDocument({ content, language })
   const editor = await window.showTextDocument(document)
 
   await editor.edit(eb => { eb.setEndOfLine(lineEnding) })
@@ -26,13 +25,14 @@ async function runTest(content, language = 'markdown', lineEnding = LF, secondLa
   setCursorPosition(editor, content.length, 0)
 
   if (secondLanguage) {
-    document = await languages.setTextDocumentLanguage(document, secondLanguage)
-    await commands.executeCommand('type', { text: ' ' })
-    await new Promise(resolve => setTimeout(resolve, WAIT_TIME))
+    await languages.setTextDocumentLanguage(document, secondLanguage)
+    await wait(WAIT_TIME)
   }
 
   await commands.executeCommand('type', { text: RETURN })
+
   const newText = document.getText().split(RETURN).pop()
+
   await commands.executeCommand('workbench.action.closeAllEditors')
 
   return newText
@@ -40,6 +40,11 @@ async function runTest(content, language = 'markdown', lineEnding = LF, secondLa
 
 function setCursorPosition(editor, column, line) {
   editor.selection = new Selection(line, column, line, column)
+}
+
+async function wait(milliseconds) {
+  // eslint-disable-next-line no-promise-executor-return
+  return await new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
 suite('Function Tests', () => {
@@ -63,65 +68,65 @@ suite('Execution Tests', () => {
 
   test('Inserts dash bullet point LF', async () => {
     const newLine = await runTest(testDepthZero)
-    const index = testDepthZero.indexOf('-')
-    assert.strictEqual(newLine, testDepthZero.slice(0, index + 2), 'New line should start with dash bullet')
+    const index = testDepthZero.indexOf('-') + 2
+    assert.strictEqual(newLine, testDepthZero.slice(0, index), 'New line should start with dash bullet')
   })
 
   test('Inserts dash bullet point CRLF', async () => {
     const newLine = await runTest(testDepthZero, 'markdown', CRLF)
-    const index = testDepthZero.indexOf('-')
-    assert.strictEqual(newLine, testDepthZero.slice(0, index + 2), 'New line should start with dash bullet')
+    const index = testDepthZero.indexOf('-') + 2
+    assert.strictEqual(newLine, testDepthZero.slice(0, index), 'New line should start with dash bullet')
   })
 
   test('Inserts star bullet point LF', async () => {
     const newLine = await runTest(testDepthOne)
-    const index = testDepthOne.indexOf('*')
-    assert.strictEqual(newLine, testDepthOne.slice(0, index + 2), 'New line should start with star bullet')
+    const index = testDepthOne.indexOf('*') + 2
+    assert.strictEqual(newLine, testDepthOne.slice(0, index), 'New line should start with star bullet')
   })
 
   test('Inserts star bullet point CRLF', async () => {
     const newLine = await runTest(testDepthOne, 'markdown', CRLF)
-    const index = testDepthOne.indexOf('*')
-    assert.strictEqual(newLine, testDepthOne.slice(0, index + 2), 'New line should start with star bullet')
+    const index = testDepthOne.indexOf('*') + 2
+    assert.strictEqual(newLine, testDepthOne.slice(0, index), 'New line should start with star bullet')
   })
 
   test('Inserts plus bullet point LF', async () => {
     const newLine = await runTest(testDepthTwo)
-    const index = testDepthTwo.indexOf('+')
-    assert.strictEqual(newLine, testDepthTwo.slice(0, index + 2), 'New line should start with plus bullet')
+    const index = testDepthTwo.indexOf('+') + 2
+    assert.strictEqual(newLine, testDepthTwo.slice(0, index), 'New line should start with plus bullet')
   })
 
   test('Inserts plus bullet point CRLF', async () => {
     const newLine = await runTest(testDepthTwo, 'markdown', CRLF)
-    const index = testDepthTwo.indexOf('+')
-    assert.strictEqual(newLine, testDepthTwo.slice(0, index + 2), 'New line should start with plus bullet')
+    const index = testDepthTwo.indexOf('+') + 2
+    assert.strictEqual(newLine, testDepthTwo.slice(0, index), 'New line should start with plus bullet')
   })
 
   test('Stops when going from markdown to text', async () => {
     const newLine1 = await runTest(testDepthZero)
     const newLine2 = await runTest(testDepthZero, 'plaintext')
-    const index = testDepthZero.indexOf('-')
+    const index = testDepthZero.indexOf('-') + 2
 
-    assert.strictEqual(newLine1, testDepthZero.slice(0, index + 2), 'New line should start with dash bullet')
+    assert.strictEqual(newLine1, testDepthZero.slice(0, index), 'New line should start with dash bullet')
     assert.strictEqual(newLine2, '', 'New line should be blank')
   })
 
   test('Starts up after going from text to markdown', async () => {
     const newLine1 = await runTest(testDepthZero, 'plaintext')
     const newLine2 = await runTest(testDepthZero)
-    const index = testDepthZero.indexOf('-')
+    const index = testDepthZero.indexOf('-') + 2
 
     assert.strictEqual(newLine1, '', 'New line should be blank')
-    assert.strictEqual(newLine2, testDepthZero.slice(0, index + 2), 'New line should start with dash bullet')
+    assert.strictEqual(newLine2, testDepthZero.slice(0, index), 'New line should start with dash bullet')
   })
 
   test('Keeps working when going from markdown to markdown', async () => {
     const newLine1 = await runTest(testDepthZero)
     const newLine2 = await runTest(testDepthZero)
-    const index = testDepthZero.indexOf('-')
+    const index = testDepthZero.indexOf('-') + 2
 
-    assert.strictEqual(newLine1, testDepthZero.slice(0, index + 2), 'New line should start with dash bullet')
-    assert.strictEqual(newLine2, testDepthZero.slice(0, index + 2), 'New line should start with dash bullet')
+    assert.strictEqual(newLine1, testDepthZero.slice(0, index), 'New line should start with dash bullet')
+    assert.strictEqual(newLine2, testDepthZero.slice(0, index), 'New line should start with dash bullet')
   })
 
   test('Stops when document language is changed from markdown to text', async () => {
@@ -131,7 +136,7 @@ suite('Execution Tests', () => {
 
   test('Starts when document language is changed from text to markdown', async () => {
     const newLine = await runTest(testDepthZero, 'plaintext', LF, 'markdown')
-    const index = testDepthZero.indexOf('-')
-    assert.strictEqual(newLine, testDepthZero.slice(0, index + 2), 'New line should start with dash bullet')
+    const index = testDepthZero.indexOf('-') + 2
+    assert.strictEqual(newLine, testDepthZero.slice(0, index), 'New line should start with dash bullet')
   })
 })
